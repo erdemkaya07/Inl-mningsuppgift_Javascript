bcda790fc2409173cfe72a2ec616d8df9650d89c
Erdem Kaya <kerdem@live.com>
2024-10-25 08:08:50 +0200

    Project CSS update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import '../src/assets/css/colors.css'
+import '../src/assets/css/main.css'
+import '../src/assets/css/media.css'
+import '../src/assets/css/utilities.css'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <div className="container">
+      <h1>erdem</h1>
+    </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 /* import { StrictMode } from 'react' */
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '../src/assets/css/colors.css'
+import '../src/assets/css/main.css'
+import '../src/assets/css/media.css'
+import '../src/assets/css/utilities.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
