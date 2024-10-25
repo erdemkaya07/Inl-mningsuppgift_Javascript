eaac2c677b2e8b81938e4f6450f59f8176fb4bda
Erdem Kaya <kerdem@live.com>
2024-10-25 15:53:53 +0200

    Navbar END

--- src/components/1Nav/Nav.jsx
+++ src/components/1Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import LogoLight from "../../assets/images/logo-img.svg";
 import LogoDark from "../../assets/images/siliconlogo-dark.svg"
 import userIcon from "../../assets/images/user-icon.png";
@@ -8,21 +8,27 @@ import Toggle from "./Toggle";
 
 function Nav() {
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const mobileMenu = () => {
+    setIsOpen((open) => !open)
+  };
+
   return (
     <nav aria-label="main-navigation">
       <div className="container">
         <div className="navbar flex justify-space-between align-items-center mb-1">
           <div className="navbar-right flex align-items-center">
-            <img className="show-light logo-light mr-2" src={LogoLight} alt="Silicon logo" />
-            <img className="show-dark logo-light mr-2" src={LogoDark} alt="Silicon logo" />
-            <ul className="main-menu flex justify-center">
-              <li>
-                <a href="#" className="navbar-links mr-1 weigt-semi-bold">Features</a>
-              </li>
-              <li>
-                <a href="#" className="navbar-links weigt-semi-bold">Contact</a>
-              </li>
-            </ul>
+            <img className="show-light mr-2" src={LogoLight} alt="Silicon logo" />
+            <img className="show-dark mr-2" src={LogoDark} alt="Silicon logo" />
+              <ul id="main-menu" className={`main-menu flex justify-center ${isOpen ? "is-open" : ""}`} >
+                <li>
+                  <a href="#" className="nav-links mr-1 weigt-semi-bold">Features</a>
+                </li>
+                <li>
+                  <a href="#" className="nav-links weigt-semi-bold">Contact</a>
+                </li>
+              </ul>
           </div>
           <div className="navbar-left flex">
             <Toggle />
@@ -30,7 +36,7 @@ function Nav() {
                 <img src={userIcon} alt="" />
                 <span className="pl-0 weigt-semi-bold">Sign in / up</span>
               </button>
-              <button className="btn-mobile ml-3"><i class="fa-regular fa-bars"></i></button>
+              <button className="btn-mobile ml-3" onClick={mobileMenu}><i className="fa-regular fa-bars"></i></button>
           </div>
         </div>
       </div>
